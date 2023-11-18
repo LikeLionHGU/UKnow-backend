69113c032e3e2428a66ff446ab9a4204abe8023f
김동규 <kdg1941@naver.com>
2023-11-19 00:13:29 +0900

    init: 프로젝트 초기 세팅

---
+++ uknow/src/main/java/hgu/likelion/uknow/controller/HisnetLoginController.java
@@ -0,0 +1,7 @@
+package hgu.likelion.uknow.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HisnetLoginController {
+}
---
+++ uknow/src/test/java/hgu/likelion/uknow/UknowApplicationTests.java
@@ -0,0 +1,13 @@
+package hgu.likelion.uknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UknowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
