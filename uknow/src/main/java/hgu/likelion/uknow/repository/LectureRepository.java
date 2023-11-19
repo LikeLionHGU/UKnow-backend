452c860d4aa62aa89242c36dea6448af8053c144
김동규 <kdg1941@naver.com>
2023-11-19 15:11:34 +0900

    feat: 과목 파싱 데이터 DB 저장 기능 구현

--- uknow/src/main/java/hgu/likelion/uknow/repository/LectureRepository.java
+++ uknow/src/main/java/hgu/likelion/uknow/repository/LectureRepository.java
@@ -2,6 +2,12 @@ package hgu.likelion.uknow.repository;
 
 import hgu.likelion.uknow.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LectureRepository extends JpaRepository<Lecture, String> {
+    @Query("select r from Lecture r where r.code = :code and r.isEnglish =:isEnglish")
+    Lecture findByCodeAndEnglish(@Param("code") String code, @Param("isEnglish") Boolean isEnglish);
 }
--- uknow/src/main/java/hgu/likelion/uknow/service/LectureService.java
+++ uknow/src/main/java/hgu/likelion/uknow/service/LectureService.java
@@ -1,9 +1,17 @@
 package hgu.likelion.uknow.service;
 
+import hgu.likelion.uknow.entity.Lecture;
 import hgu.likelion.uknow.repository.LectureRepository;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +19,61 @@ public class LectureService {
     private final LectureRepository lectureRepository;
 
     @Transactional
-    public void addLecture() {
+    public void addLecture(List<List<String>> lectureList) {
 
+        for(int i = 1; i < lectureList.size(); i++) {
+            String type = lectureList.get(i).get(0);
+            String code = lectureList.get(i).get(1);
+            String name = lectureList.get(i).get(3);
+
+            Double credit;
+
+            if(lectureList.get(i).get(4).equals(".5")) {
+                credit = 0.5;
+            } else {
+                credit = Double.valueOf(Long.valueOf(lectureList.get(i).get(4)));
+            }
+
+            Boolean isEnglish;
+            if(lectureList.get(i).get(10).equals("100%")) {
+                isEnglish = true;
+            } else {
+                isEnglish = false;
+            }
+            String nonMajor = lectureList.get(i).get(11);
+
+            Lecture lecture = lectureRepository.findByCodeAndEnglish(code, isEnglish);
+
+            if(lecture == null) {
+                Lecture newLecture = Lecture.toAdd(code, name, credit, isEnglish, nonMajor, type);
+                lectureRepository.save(newLecture);
+            }
+        }
+    }
+
+    @Transactional
+    public List<List<String>> parseLecture(String html) {
+        Document document = Jsoup.parse(html);
+        List<List<String>> currentTable = new ArrayList<>();
+
+        Elements tables = document.select("table");
+
+        for(Element table : tables) {
+            Elements rows = table.select("tr");
+
+
+            for(Element row : rows) {
+                List<String> rowData = new ArrayList<>();
+                Elements cols = row.select("td");
+
+                for(Element col : cols) {
+                    rowData.add(col.text().trim());
+                }
+                currentTable.add(rowData);
+            }
+
+        }
+
+        return currentTable;
     }
 }
