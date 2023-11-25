e3d6cde3cfa98f0c6a4b8c8c9071d8fbc1dd9164
김동규 <kdg1941@naver.com>
2023-11-26 03:25:06 +0900

    feat: 사용자 이수 목록 로딩 기능 수정(세부적인 요소 적용)

--- uknow/src/main/java/hgu/likelion/uknow/common/LectureType.java
+++ uknow/src/main/java/hgu/likelion/uknow/common/LectureType.java
@@ -1,5 +1,6 @@
 package hgu.likelion.uknow.common;
 
 public enum LectureType {
-    faith, leaderShip, english, professionalCulture, BSM, ICT, culture, major
+    faith, leaderShip, english, professionalCulture, BSM, ICT, culture, major,
+    chapel, firstFaith, secondFaith, firstWorld, secondWorld, team, service, edu, BSMSet, majorEssential, majorChoice, design
 }
